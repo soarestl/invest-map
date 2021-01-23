e5969e8121e9b9488a95dea737906918e22485eb
Thiago Soares <thiago.lima.soares258@gmail.com>
2021-01-23 07:41:25 -0300

    first compilation

---
+++ build/CMakeFiles/invest_map.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/invest_map.dir/src/main.c.o"
+  "invest_map.pdb"
+  "invest_map"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/invest_map.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
